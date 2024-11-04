49a7a5e9d9957f95b719f5d4ba361a043b705c81
Baskervir <persona903@gmail.com>
2024-11-05 00:18:42 +0900

    added t2

--- src/Biz_test.java
+++ src/Biz_test.java
@@ -5,11 +5,11 @@ public class Biz_test {
     @Test
     public void t1() {
         String emp1_empN = "E01";
-        String emp1_nm = "정민";
+        String emp1_nm = "정민1";
         long emp1_sal = 10000000;
 
         String emp2_empN = "E02";
-        String emp2_nm = "현호";
+        String emp2_nm = "정민2";
         long emp2_sal = 30000000;
 
         long sum = emp1_sal + emp2_sal;
@@ -23,5 +23,23 @@ public class Biz_test {
     }
 
     @Test
-    
+    public void t2() {
+        String[] empCdArray = new String[2];
+        empCdArray = new String[] {"E01", "E02"};
+
+        String[] empNmArray = new String[2];
+        empNmArray[0] = "정민1";
+        empNmArray[1] = "정민2";
+
+        long[] salArray = new long[] {10000000, 30000000};
+
+        for (int i = 0; i < empCdArray.length; i++) {
+            String empCd = empCdArray[i];
+            String empNm = empNmArray[i];
+            long sal = salArray[i];
+
+            String empStr = empCd + " " + empNm + " " + sal;
+            System.out.println(empStr);
+        }
+    }
 }
