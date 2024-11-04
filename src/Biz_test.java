51da4b4d5a8a0719b23b6473411ead1a215bcce6
Baskervir <persona903@gmail.com>
2024-11-05 00:26:12 +0900

    added t3, t4, t5

--- src/Biz_test.java
+++ src/Biz_test.java
@@ -42,4 +42,45 @@ public class Biz_test {
             System.out.println(empStr);
         }
     }
+
+    @Test
+    public void t3() {
+        Emp emp1 = new Emp("E01", "정민1", 10000000);
+        Emp emp2 = new Emp("E02", "정민2", 30000000);
+
+        String emp1Str = emp1.toString();
+        System.out.println(emp1Str);
+
+        String emp2Str = emp2.toString();
+        System.out.println(emp2Str);
+    }
+
+    @Test
+    public void t4() {
+        Emp emp1 = new Emp("E01", "정민1", 10000000);
+        Emp emp2 = new Emp("E02", "정민2", 30000000);
+
+        Emp[] empList = new Emp[2];
+        empList[0] = emp1;
+        empList[1] = emp2;
+
+        long sum = 0;
+        for (Emp emp : empList) {
+            sum = sum + emp.getSal();
+            System.out.println(sum);
+        }
+    }
+
+    @Test
+    public void t5() {
+        Dpt[] dptArray = new Dpt[3];
+        dptArray[0] = new Dpt("C0");
+        dptArray[1] = new Dpt("S0");
+        dptArray[2] = new Dpt("A1");
+
+        for (Dpt dpt : dptArray) {
+            String dptNm = dpt.getDptNm();
+            System.out.println(dptNm);
+        }
+    }
 }
