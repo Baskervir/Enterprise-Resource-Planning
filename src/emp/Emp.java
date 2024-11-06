ca89b222590a156952a87684afa00ccf42f51da4
Baskervir <persona903@gmail.com>
2024-11-06 17:02:40 +0900

    .

--- src/emp/Emp.java
+++ src/emp/Emp.java
@@ -1,10 +1,13 @@
 package emp;
 
+import dpt.Dpt;
+import dpt.DptM;
+
 public class Emp {
-    private String empN;
-    private String nm;
-    private long sal;
-    public String dptCd;
+    private String empN = null;
+    private String nm = null;
+    private long sal = 0;
+    private String dptCd = "C0";
 
     public Emp(String inputEmpN, String inputNm, long inputSal) {
         this.empN = inputEmpN;
@@ -25,6 +28,23 @@ public class Emp {
     }
 
     public String toString() {
+        return this.empN + " " + this.nm + " " + sal;
+    }
+
+    public void setDptCd(String dptCd){
+        DptM dptM = new DptM();
+        Dpt dpt = dptM.getDpt(dptCd);
+        if (dpt == null) {
+            throw new RuntimeException(dptCd + " 는 존재하지 않습니다.");
+        }
+        this.dptCd = dptCd;
+    }
+
+    public String getDptCd() {
+        return this.dptCd;
+    }
+
+    public String getProfile() {
         return this.empN + " " + this.nm + " " + sal;
     }
 }
